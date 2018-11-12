0e25ebfdfdc27ac3ddfd8fc7a631bbcd4b3fcd7b
Marcelo Cárdenas <cardenas.marcelo@principal.com>
2018-11-12 13:11:00 -0300

    initial commit

---
+++ src/main/java/cl/principal/webpaytesting/WebpaytestingApplication.java
@@ -0,0 +1,12 @@
+package cl.principal.webpaytesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpaytestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpaytestingApplication.class, args);
+	}
+}
